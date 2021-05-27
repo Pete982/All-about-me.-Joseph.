d4446a683da2446f4db9e0575bc32945f992ea44
Pete982 <84607563+Pete982@users.noreply.github.com>
2021-05-26 21:55:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor() {
+    document.getElementById("heading1").style.color = "orange";
+}
+
+function changecolorwhite() {
+    document.getElementById("para").style.color = "white";
+}
